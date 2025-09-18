95fe1dc037327ccad6fac3184993036588919b2a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-09-18 07:07:01 +0200

    feat(evaluation): add integration with cratesio

--- crates/pollux/src/core.rs
+++ crates/pollux/src/core.rs
@@ -2,12 +2,24 @@
 // SPDX-License-Identifier: MIT
 
 use crate::infra::{ReproducibleBuildsEvaluator, TrustedPublishingEvaluator};
+use std::fmt::Display;
 
-#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub struct CrateInfo {
     pub name: String,
     pub version: String,
+}
+
+impl CrateInfo {
+    pub fn new(name: String, version: String) -> Self {
+        Self { name, version }
+    }
+}
+
+impl Display for CrateInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("pkg:cargo/{}@{}", self.name, self.version))
+    }
 }
 
 #[allow(dead_code)]
@@ -30,7 +42,6 @@ pub trait TruthfulnessEvaluation {
     async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<bool>;
 }
 
-#[allow(dead_code)]
 pub struct TruthfulnessEvaluator {
     trusted_publishing_evaluator: TrustedPublishingEvaluator,
     reproducible_builds_evaluator: ReproducibleBuildsEvaluator,
@@ -38,7 +49,17 @@ pub struct TruthfulnessEvaluator {
 
 #[allow(dead_code)]
 impl TruthfulnessEvaluator {
-    async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<TruthfulnessVerification> {
+    pub fn new(
+        trusted_publishing_evaluator: TrustedPublishingEvaluator,
+        reproducible_builds_evaluator: ReproducibleBuildsEvaluator,
+    ) -> Self {
+        Self {
+            trusted_publishing_evaluator,
+            reproducible_builds_evaluator,
+        }
+    }
+
+    pub async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<TruthfulnessVerification> {
         let uses_trusted_publishing = self.trusted_publishing_evaluator.evaluate(crate_info).await?;
         let has_reproduced_build = self.reproducible_builds_evaluator.evaluate(crate_info).await?;
 
--- crates/pollux/src/infra.rs
+++ crates/pollux/src/infra.rs
@@ -1,50 +1,82 @@
 // Copyright 2025 Dotanuki Labs
 // SPDX-License-Identifier: MIT
 
+pub mod cratesio;
+
 use crate::core::{CrateInfo, TruthfulnessEvaluation};
+use crate::infra::cratesio::CratesIOEvaluator;
+use reqwest::Client;
+
+pub type HTTPClient = Client;
 
 #[allow(dead_code)]
-pub struct CratesIOApiClient;
-
-#[allow(dead_code)]
-pub struct OssRebuildBucketClient;
+pub struct OssRebuildEvaluator;
 
 #[allow(dead_code)]
 pub enum TrustedPublishingEvaluator {
-    FromCratesIO(CratesIOApiClient),
+    FromCratesIO(CratesIOEvaluator),
     #[cfg(test)]
-    Fake(Vec<CrateInfo>),
+    Fake(FakeTruthfulnessEvaluator),
 }
 
 impl TruthfulnessEvaluation for TrustedPublishingEvaluator {
     async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<bool> {
         match self {
-            TrustedPublishingEvaluator::FromCratesIO(_) => {
-                println!("{:?}", crate_info);
-                Ok(true)
-            },
+            TrustedPublishingEvaluator::FromCratesIO(evaluator) => evaluator.evaluate(crate_info).await,
             #[cfg(test)]
-            TrustedPublishingEvaluator::Fake(crates) => Ok(crates.contains(crate_info)),
+            TrustedPublishingEvaluator::Fake(evaluator) => evaluator.evaluate(crate_info).await,
         }
     }
 }
 
 #[allow(dead_code)]
 pub enum ReproducibleBuildsEvaluator {
-    FromOssRebuild(OssRebuildBucketClient),
+    FromOssRebuild(OssRebuildEvaluator),
     #[cfg(test)]
     Fake(Vec<CrateInfo>),
 }
 
+#[allow(unused_variables)]
 impl TruthfulnessEvaluation for ReproducibleBuildsEvaluator {
     async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<bool> {
         match self {
-            ReproducibleBuildsEvaluator::FromOssRebuild(_) => {
-                println!("{:?}", crate_info);
-                Ok(true)
-            },
+            ReproducibleBuildsEvaluator::FromOssRebuild(_) => Ok(true),
             #[cfg(test)]
             ReproducibleBuildsEvaluator::Fake(crates) => Ok(crates.contains(crate_info)),
         }
+    }
+}
+
+pub mod factories {
+    use crate::infra::cratesio::CratesIOEvaluator;
+    use crate::infra::{HTTPClient, TrustedPublishingEvaluator};
+    use reqwest::header;
+    use std::sync::{Arc, LazyLock};
+
+    pub static HTTP_CLIENT: LazyLock<Arc<HTTPClient>> = LazyLock::new(|| {
+        let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(header::USER_AGENT, header::HeaderValue::from_str(&user_agent).unwrap());
+
+        let client = HTTPClient::builder().default_headers(headers).build().unwrap();
+        Arc::new(client)
+    });
+
+    static CRATES_IO_API: &str = "https://crates.io";
+
+    pub fn trusted_publishing_evaluator() -> TrustedPublishingEvaluator {
+        let delegate = CratesIOEvaluator::new(CRATES_IO_API.to_string(), HTTP_CLIENT.clone());
+        TrustedPublishingEvaluator::FromCratesIO(delegate)
+    }
+}
+
+#[cfg(test)]
+pub struct FakeTruthfulnessEvaluator(Vec<CrateInfo>);
+
+#[cfg(test)]
+impl TruthfulnessEvaluation for FakeTruthfulnessEvaluator {
+    async fn evaluate(&self, crate_info: &CrateInfo) -> anyhow::Result<bool> {
+        Ok(self.0.contains(crate_info))
     }
 }
